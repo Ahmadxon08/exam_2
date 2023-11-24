5f32b14a6e08a4888df5b1f16fde014c85b95bcb
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2023-11-24 09:23:37 +0500

    exam-2

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ function toggleNavbar() {
 }
 
 document.getElementById("menu-btn").addEventListener("click", toggleNavbar);
+
+// backtopp
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = "20px";
+  } else {
+    backtop.style.bottom = "-50px";
+  }
+}
